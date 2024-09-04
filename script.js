a57be4e4419c41b047fbd8a2ffc2d6528fdb8ebf
Christopher <117647249+christophercolombo@users.noreply.github.com>
2024-09-04 15:38:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = `Em 2024, ${nome}`;
+    caixaPerguntas.textContent = `Em StarWars, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
